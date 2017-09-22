d7cbbc57f68cc7cb400d7c201297dd336894eb58
Elie Schoppik <eschoppik@gmail.com>
2017-09-22 11:23:46 -0700

    getting ready to REDUXXXX

--- src/TodoList.js
+++ src/TodoList.js
@@ -5,14 +5,34 @@ import Todo from "./Todo";
 class TodoList extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      task: ""
+    };
+    this.handleAdd = this.handleAdd.bind(this);
+    this.handleChange = this.handleChange.bind(this);
   }
+  handleAdd() {}
+
+  handleChange(e) {
+    this.setState({
+      [e.target.name]: e.target.value
+    });
+  }
+
   render() {
     let todos = this.props.todos.map(function(val) {
-      return <Todo task={val.task} key={val.id} />;
+      return <Todo task={val.task} name="task" key={val.id} />;
     });
 
     return (
       <div>
+        <form onSubmit={this.handleAdd}>
+          <input
+            type="text"
+            value={this.state.task}
+            onChange={this.handleChange}
+          />
+        </form>
         <h1>See the todos!</h1>
         {todos}
       </div>
