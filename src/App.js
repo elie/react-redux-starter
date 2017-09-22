a852c0b08405cf6a64c0d77f23f90abc39e87f48
Elie Schoppik <eschoppik@gmail.com>
2017-09-22 10:54:03 -0700

    working starter

--- src/App.js
+++ src/App.js
@@ -1,19 +1,28 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import rootReducer from "./rootReducer";
+import { createStore, compose } from "redux";
+import { Provider } from "react-redux";
+
+const mySuperDuperStore = createStore(
+  rootReducer,
+  compose(
+    typeof window === "object" &&
+    typeof window.devToolsExtension !== "undefined"
+      ? window.devToolsExtension()
+      : f => f
+  )
+);
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+      <Provider store={mySuperDuperStore}>
+        <div>
+          <h1>Welcome to our app!</h1>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      </Provider>
     );
   }
 }
